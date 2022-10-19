b5cd2b4c3274e9e060ed8e3a6b512f0eb689617f
Bello Emmanuel <nathemmanuel.03@gmail.com>
2022-10-19 14:11:53 +0100

    Remove print.c and update the printf code in the original file

--- main.h
+++ main.h
@@ -2,10 +2,9 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_char(va_list c);
-int print_strings(va_list s);
 
 #endif
