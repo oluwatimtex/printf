b98508bc6d6fee80ef51cdee5f14671d91bbf7df
root <root@Angry-bird.localdomain>
2022-10-22 01:21:42 +0100

    updated

--- _printf.c
+++ _printf.c
@@ -1,6 +1,30 @@
 #include "main.h"
 
 /**
+ * check_for_specifiers - checks if there is a valid format specifier
+ * @format: possible format specifier
+ *
+ * Return: pointer to valid function or NULL
+ */
+static int (*check_for_specifiers(const char *format))(va_list)
+{
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{NULL, NULL}
+	};
+
+	for (i = 0; p[i].t != NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+}
+/**
  * _printf - prints the characters given to it but
  * takes note of some format specifiers
  * @format: This is the string that's going to be checked through
--- main.h
+++ main.h
@@ -3,8 +3,20 @@
 
 #include <stdarg.h>
 #include <stdlib.h>
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
 
 #endif
